b6fd529387118cb929b40bac2c91f351c2b5f4f8
evemak <136145873+evemak@users.noreply.github.com>
2025-11-05 14:44:42 -0500

    fix

--- src/pylloween/__init__.py
+++ src/pylloween/__init__.py
@@ -15,6 +15,8 @@ from .horrors import get_horror, get_movie_idea
 from .ghosts import draw_ghost, ghost_say, ghost_story, ghost_idea
 from .pumpkin import draw_pumpkin
 from .treats import trick_or_treat, normalize_bag
+from .bat import draw_bat
+from .draw_rand import draw_random
 
 __all__ = [
     "get_horror",
